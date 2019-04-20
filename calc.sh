01d9b1e191c3a4b3d4821c201b2200601fd4d93d
sanjaypj <sanjaypjayan2000@gmail.com>
2019-04-20 13:11:40 +0530

    calc proj

--- calc.sh
+++ calc.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-echo "Enter First Number :: "
-read num1
-echo "Enter Second Number :: "
-read num2
+WANT_TO_CONT=1
+
+while [ $WANT_TO_CONT == 1 ]; do
+
+	echo -n "ENTER FIRST NUMBER :: "
+	read NUM1
+	echo -n "ENTER SECOND NUMBER :: "
+	read NUM2
+
+	echo "1) ADDITION"
+	echo "2) SUBSTRACTION"
+	echo "3) MULTIPLICATION"
+	echo "4) DIVISION"
+
+	echo -n "CHOOSE YOUR OPTION :: "
+	read OPTION
+
+	case $OPTION in
+		1 ) ANS=`expr $NUM1 + $NUM2`
+			echo "ANS :: $ANS";;
+		2 ) ANS=`expr $NUM1 - $NUM2`
+			echo "ANS :: $ANS";;
+		3 ) ANS=`expr $NUM1 \* $NUM2`
+			echo "ANS :: $ANS";;
+		4 ) ANS=`expr $NUM1 / $NUM2`
+			echo "ANS :: $ANS";;
+		* ) echo -n "INVALID OPTION"
+	esac
+
+	echo -n "DO YOU WANT TO CONTINUE [1/0] :: "
+	read WANT_TO_CONT;
+
+done
 
--- learn.sh
+++ learn.sh
@@ -1,54 +1,62 @@
 #!/bin/bash
 
-# echo "What is your name : "
-# read PERSON
-# echo "Hello, $PERSON"
+
+#READ AND PRINT
+echo "What is your name : "
+read PERSON
+echo "Hello, $PERSON"
 
 
-# NAME="sanjay pj"
-# VAR1=12
+#VARIABLES
+NAME="sanjay pj"
+VAR1=12
 
-# echo "$NAME, $VAR1"
+echo "$NAME, $VAR1"
 
-#echo "File Name: $0"
-#echo "First Parameter : $1"
-#echo "Second Parameter : $2"
-#echo "Quoted Values: $@"
-#echo "Quoted Values: $*"
-#echo "Total Number of Parameters : $#"
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+#INPUT PARAMETERS
+for TOKEN in $*; do
+  echo $TOKEN
+done
 
 
-#for TOKEN in $*; do
-#   echo $TOKEN
-#done
+#ARRAY
+NAME[0]="Sam"
+NAME[1]="Smith"
+NAME[2]="Sanjay"
+echo "FIRST INDEX :: ${NAME[0]}"
+echo "SECOND INDEX :: ${NAME[1]}"
+echo "INDEX :: ${NAME[*]}"
 
 
-#NAME[0]="Sam"
-#NAME[1]="Smith"
-#NAME[2]="Sanjay"
-#echo "FIRST INDEX :: ${NAME[0]}"
-#echo "SECOND INDEX :: ${NAME[1]}"
-#echo "INDEX :: ${NAME[*]}"
+#ARITHMETIC EXPR
+VAR1=`expr 2 + 2`
+echo "TOTAL VALUE :: $VAR1"
 
-#VAR1=`expr 2 + 2`
-#echo "TOTAL VALUE :: $VAR1"
+VAR2=`expr 22 - 4`
+echo "VALUE :: $VAR2"
 
-#VAR2=`expr 22 - 4`
-#echo "VALUE :: $VAR2"
+VAR3=`expr 2 \* 4`
+echo "VALUE :: $VAR3"
 
-#VAR3=`expr 2 \* 4`
-#echo "VALUE :: $VAR3"
+VAR4=`expr 22 / 2`
+echo "VALUE :: $VAR4"
 
-#VAR4=`expr 22 / 2`
-#echo "VALUE :: $VAR4"
-
-#VAR5=`expr 22 % 6`
-#echo "VALUE :: $VAR5"
+VAR5=`expr 22 % 6`
+echo "VALUE :: $VAR5"
 
 VAR1="Sanjay PJ"
 VAR2=$VAR1
 echo "$VAR2"
 
+
+#CONDITIONAL STATEMETNS
 COND1=$[ 5 == 5 ]
 echo "$COND1"
 
@@ -58,6 +66,7 @@ echo "$COND1"
 VAR1=22;
 VAR2=33;
 
+#IF CONDITIONS
 if [ $VAR1 -eq $VAR2 ]; then
 	echo "TRUE"
 else
@@ -108,16 +117,22 @@ else
         echo "FALSE"
 fi
 
+
+#WHILE LOOP
 while [ true ]; do
 	echo "HELLO WORLD"
 	break
 done
 
+
+#FOR LOOP
 for i in {0..10..1}; do
 	echo "Welcome $i times"
 done
 
 
+
+#CASE STATEMETNS
 echo -n "Enter the name of a country: "
 read COUNTRY
 
